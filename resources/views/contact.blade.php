7e70c7f042dc65fffe679d52e4dae396022d57d6
unknown <chellalmostafa@gmail.com>
2024-01-29 09:05:38 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <h2>This Contact Page</h2>
+    <a href="{{route('about.page')}}"> About </a>
+</head>
+<body>
+    
+</body>
+</html>
